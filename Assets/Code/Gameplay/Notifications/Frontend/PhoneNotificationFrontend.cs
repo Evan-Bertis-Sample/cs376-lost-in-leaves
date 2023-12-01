fc7c2c0f3e1130c91848486c07268f9ec8fc7592
Shrimp-Chip <esample21@gmail.com>
2023-11-30 20:29:25 -0600

    iPhone type beat

---
+++ Assets/Code/Gameplay/Notifications/Frontend/PhoneNotificationFrontend.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LostInLeaves.Notifications;
+
+namespace LostInLeaves.Notifications.Frontend
+{
+    public class PhoneNotificationFrontend : MonoBehaviour
+    {
+
+    }
+}
