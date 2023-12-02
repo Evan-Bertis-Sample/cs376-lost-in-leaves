58bb7cc11a44a618773f13ca753c89fb99297fbc
Shrimp-Chip <esample21@gmail.com>
2023-12-01 23:16:51 -0600

    Added repeat and random delays to sound interactable

--- Assets/Code/Gameplay/Enviornment/SoundInteractable.cs
+++ Assets/Code/Gameplay/Enviornment/SoundInteractable.cs
@@ -9,10 +9,22 @@ namespace LostInLeaves
 {
     public class SoundInteractable : InteractableObject
     {
+        [System.Serializable]
+        internal struct Range
+        {
+            public float Min;
+            public float Max;
+        }
+
         [Header("Sound Settings")]
         [SerializeField, AudioPath] private string _audioPath;
         [SerializeField] private AudioOverride _audioOverride;
         [SerializeField] private Transform _audioTarget;
+
+        [Header("Randomization Settings")]
+        [SerializeField] private Range _entranceDelayRange;
+        [SerializeField] private bool _repeat;
+        [SerializeField] private Range _repeatDelayRange;
 
         [GlobalDefault] private AudioManager _audioManager;
         private bool _isPlaying = false;
@@ -23,8 +35,30 @@ namespace LostInLeaves
         {
             if (_isPlaying) return;
             if (_audioManager == null) DependencyInjector.InjectDependencies(this);
+            StartCoroutine(PlaySoundInitial());
+        }
 
+        protected override void Interact(Interactor interactor)
+        {
+            if (!_repeat) return;
+            if (_isPlaying) return;
+            StartCoroutine(PlaySoundRepeat());
+        }
+
+        private IEnumerator PlaySoundInitial()
+        {
             _isPlaying = true;
+            yield return new WaitForSeconds(Random.Range(_entranceDelayRange.Min, _entranceDelayRange.Max));
+            _audioManager.PlayOneShot(_audioPath, _targetPosition, _audioOverride, callback =>
+            {
+                callback.OnAudioEnd += source => _isPlaying = false;
+            });
+        }
+
+        private IEnumerator PlaySoundRepeat()
+        {
+            _isPlaying = true;
+            yield return new WaitForSeconds(Random.Range(_repeatDelayRange.Min, _repeatDelayRange.Max));
             _audioManager.PlayOneShot(_audioPath, _targetPosition, _audioOverride, callback =>
             {
                 callback.OnAudioEnd += source => _isPlaying = false;
